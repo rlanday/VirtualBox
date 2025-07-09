afaf069ff31b08c155c7486a26e6eca97bc9ca0e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-07-09 17:20:33 +0000

    ValKit/FsPerf: Limit the --many-files value to 2 million. bugref:10925
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@109817 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/ValidationKit/utils/fs/FsPerf.cpp
+++ src/VBox/ValidationKit/utils/fs/FsPerf.cpp
@@ -6600,9 +6600,14 @@ int main(int argc, char *argv[])
 #undef CASE_OPT
 
             case kCmdOpt_ManyFiles:
-                g_fManyFiles = ValueUnion.u32 > 0;
-                g_cManyFiles = ValueUnion.u32;
-                break;
+                if (ValueUnion.u32 <= _256K * 8) /* Limit bitmap size to 256KB to avoid running out of stack. */
+                {
+                    g_fManyFiles = ValueUnion.u32 > 0;
+                    g_cManyFiles = ValueUnion.u32;
+                    break;
+                }
+                RTTestFailed(g_hTest, "Out of range --many-files value: %u (%#x)\n", ValueUnion.u32, ValueUnion.u32);
+                return RTTestSummaryAndDestroy(g_hTest);
 
             case kCmdOpt_NoManyFiles:
                 g_fManyFiles = false;
