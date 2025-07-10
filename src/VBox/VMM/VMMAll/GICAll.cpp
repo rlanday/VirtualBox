d4286b8260344de27b3f50beb3aea8a517847f4f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-07-10 07:40:09 +0000

    VMM/GIC: bugref:10877 LPI work-in-progress.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@109824 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/GICAll.cpp
+++ src/VBox/VMM/VMMAll/GICAll.cpp
@@ -2886,7 +2886,6 @@ DECLHIDDEN(void) gicReDistSetLpi(PPDMDEVINS pDevIns, PVMCPUCC pVCpu, uint16_t uI
 }
 
 
-
 /**
  * @interface_method_impl{PDMGICBACKEND,pfnSetSpi}
  */
@@ -3001,7 +3000,7 @@ DECL_HIDDEN_CALLBACK(int) gicSendMsi(PVMCC pVM, PCIBDF uBusDevFn, PCMSIMSG pMsi,
               ("Addr=%#RX64 offset=%#RX32\n", pMsi->Addr.u64, GITS_TRANSLATION_REG_TRANSLATER));
     STAM_COUNTER_INC(&pGicDev->StatSetLpi);
 
-    gitsSetLpi(pDevIns, &pGicDev->Gits, uDevId, uEventId, true /* fAsserted */);
+    gitsSetLpi(pVM, pDevIns, &pGicDev->Gits, uDevId, uEventId, true /* fAsserted */);
     return VINF_SUCCESS;
 }
 
