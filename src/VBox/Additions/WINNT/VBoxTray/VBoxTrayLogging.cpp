a95df009afec6911d4771359e95e921cb02fc0fc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-07-15 08:48:20 +0000

    GA/VBoxTray: Timestamps added to the logger config. bugref:10938
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@109852 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/VBoxTray/VBoxTrayLogging.cpp
+++ src/VBox/Additions/WINNT/VBoxTray/VBoxTrayLogging.cpp
@@ -142,7 +142,7 @@ int VBoxTrayLogCreate(const char *pszLogFile)
 #endif
     RTERRINFOSTATIC ErrInfo;
     int rc = RTLogCreateEx(&g_pLoggerRelease, s_szEnvVarPfx,
-                           RTLOGFLAGS_PREFIX_THREAD | RTLOGFLAGS_USECRLF,
+                           RTLOGFLAGS_PREFIX_THREAD | RTLOGFLAGS_PREFIX_TIME_PROG | RTLOGFLAGS_USECRLF,
                            s_szGroupSettings, RT_ELEMENTS(s_apszGroups), s_apszGroups, UINT32_MAX,
                            0 /*cBufDescs*/, NULL /*paBufDescs*/, RTLOGDEST_STDOUT | RTLOGDEST_USER,
                            vboxTrayLogHeaderFooter, g_cHistory, g_uHistoryFileSize, g_uHistoryFileTime,
